3eaf6acc6e65c7a50d359bc882750a58ae676a6f
Aslan <aslambek.hudajev@gmail.com>
2025-03-18 21:27:24 +0100

    fixed other small stuff

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
     name: "Rasbo Hundsalong",
-    email: "",
-    phoneForTel: "",
-    phoneFormatted: "",
+    email: "angelika.moberg.83@gmail.com",
+    phoneForTel: "+46709260870",
+    phoneFormatted: "070-926 08 70",
     address: {
-        lineOne: "",
+        lineOne: "Lejsta 30",
         lineTwo: "",
         city: "Uppsala",
-        state: "",
-        zip: "",
+        state: "Rasbo",
+        zip: "74178",
         country: "SE",
-        mapLink: "",
+        mapLink: "https://maps.app.goo.gl/McSKJizdTVeykWRr8",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook:
+            "https://www.facebook.com/people/Rasbo-Hundsalong/61550681531579/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
     domain: "https://www.rasbohundsalong.se",
