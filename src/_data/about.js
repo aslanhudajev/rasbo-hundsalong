3876487a42bbd8f5610a56f5a4e17a9bd013dce5
Aslan <aslambek.hudajev@gmail.com>
2025-04-01 00:55:39 +0200

    edited the site based on customer needs

--- src/_data/about.js
+++ src/_data/about.js
@@ -3,7 +3,7 @@ module.exports = {
     title: "Om Rasbo Hundsalong",
     text: [
         "På Rasbo Hundsalong står hundens välmående i fokus. Med stor omsorg och ett lugnt bemötande ser vi till att varje hund får den vård och omsorg den behöver. Oavsett om det gäller klippning, bad eller detaljerad pälsvård, anpassar vi oss efter hundens behov och din önskan. Vår salong i Rasbo är en plats där hundar får känna sig trygga och bli omhändertagna med kärlek och professionalism. Välkommen!",
-        "In consequat tincidunt turpis sit amet imperdiet. Praesent Class officelan nonatoureanor mauris laoreet, iaculis libero quis. Curabitur et tempus eri consequat tincidunt turpis sit amet imperdiet. Praesent nonatourean olei aptent taciti sociosqu ad litora torquent per.",
+        "Vi finns även på facebook, så följ oss gärna där med!",
     ],
     quote: "",
     quoteName: "Angelika",
